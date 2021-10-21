e6bc02200e3139371983c165ef8b02c4f9b72a11
Daniel Wijaya <dwjy27@gmail.com>
2021-10-21 11:15:00 +0700

    add cookies to youtube-dl so age restricted video can be played

--- structures/Track.js
+++ structures/Track.js
@@ -1,7 +1,7 @@
 const { createAudioResource, demuxProbe } = require('@discordjs/voice');
 const { raw } = require('youtube-dl-exec');
 const { TrackMetadata } = require('./TrackMetadata');
-
+const { ytCookies } = require('../config.json');
 /**
  * A Track represents information about a YouTube video (in this context) that can be added to a queue.
  * It contains the title and URL of the video, as well as functions onStart, onFinish, onError, that act
@@ -31,17 +31,19 @@ class Track {
      * Creates an AudioResource from this Track.
      */
     createAudioResource() {
+        const flags = {
+            o: '-',
+            q: '',
+            f: 'bestaudio[ext=webm+acodec=opus+asr=48000]/bestaudio',
+            r: '100K',
+        };
+
+        if (ytCookies) {
+            flags.cookies = ytCookies;
+        }
+
         return new Promise((resolve, reject) => {
-            const process = raw(
-                this.url,
-                {
-                    o: '-',
-                    q: '',
-                    f: 'bestaudio[ext=webm+acodec=opus+asr=48000]/bestaudio',
-                    r: '100K',
-                },
-                { stdio: ['ignore', 'pipe', 'ignore'] }
-            );
+            const process = raw(this.url, flags, { stdio: ['ignore', 'pipe', 'ignore'] });
             if (!process.stdout) {
                 reject(new Error('No stdout'));
                 return;
