f2bc97278f69f6d04609740d085fbbea6447950d
Daniel Wijaya <dwjy27@gmail.com>
2021-11-19 12:10:52 +0700

    add ability to automagically fetch fb post

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,19 +1,41 @@
-const client = require("../index");
+const { MessageEmbed } = require('discord.js');
+const client = require('../index');
+const { isValidUrl, isUrl } = require('../utils/Utility');
+const { getPostData } = require('../utils/Facebook');
 
-client.on("messageCreate", async (message) => {
-    if (
+client.on('messageCreate', async (message) => {
+    const firstWord = message.content.split()[0];
+    if (!message.author.bot && isValidUrl(firstWord) && isUrl(['facebook', 'fb'], firstWord)) {
+        const post = await getPostData(firstWord).catch((error) => console.log(error));
+        if (!post) return;
+        const embed = new MessageEmbed()
+            .setTitle(post.username)
+            .setDescription(post.post_text)
+            .setColor('#3B5998')
+            .addFields(
+                { name: 'Likes :thumbsup:', value: `${post.likes}`, inline: true },
+                { name: 'Shares :mega:', value: `${post.shares}`, inline: true },
+                { name: 'Comments :speech_left:', value: `${post.comments}`, inline: true }
+            );
+
+        if (post.user_url) embed.setURL(post.user_url);
+        if (post.images.length > 0) embed.setImage(post.images[0]);
+        if (post.timestamp) embed.setTimestamp(post.timestamp);
+        else if (post.time) embed.setTimestamp(new Date(post.time).getTime());
+        if (post.video) await message.channel.send(post.video);
+        await message.channel.send({ embeds: [embed] });
+    } else if (
         message.author.bot ||
         !message.guild ||
         !message.content.toLowerCase().startsWith(client.config.prefix)
     )
         return;
 
-    const [cmd, ...args] = message.content
-        .slice(client.config.prefix.length)
-        .trim()
-        .split(" ");
+    const [cmd, ...args] = message.content.slice(client.config.prefix.length).trim().split(' ');
 
-    const command = client.commands.get(cmd.toLowerCase()) || client.commands.find(c => c.aliases?.includes(cmd.toLowerCase()));
+    const command =
+        client.commands.get(cmd.toLowerCase()) ||
+        client.commands.find((c) => c.aliases?.includes(cmd.toLowerCase()));
 
     if (!command) return;
     await command.run(client, message, args);
---
+++ utils/Facebook.js
@@ -0,0 +1,14 @@
+const axios = require('axios').default;
+const { FacebookScrapperAPIKey } = require('../config.json');
+
+async function getPostData(url) {
+    const PAYLOAD = {
+        params: { url: url },
+        headers: { 'X-API-Key': FacebookScrapperAPIKey },
+    };
+    const BASE_URL = 'http://test.arv.cx/fb/';
+    const { data } = await axios.get(BASE_URL, PAYLOAD);
+    return data;
+}
+
+module.exports = { getPostData };
