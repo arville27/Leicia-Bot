433c751e03857fa02141085c5603c6b04079f6c7
Daniel Wijaya <dwjy27@gmail.com>
2022-04-23 18:26:00 +0700

    Restore ping message command

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,15 +1,17 @@
-// const { Message, Client } = require("discord.js");
+const { bold } = require('@discordjs/builders');
+const { Message, Client } = require('discord.js');
+const { embedResponse, reply } = require('../../utils/Utility');
 
-// module.exports = {
-//     name: "ping",
-//     aliases: ['p'],
-//     /**
-//      *
-//      * @param {Client} client
-//      * @param {Message} message
-//      * @param {String[]} args
-//      */
-//     run: async (client, message, args) => {
-//         message.channel.send(`${client.ws.ping} ws ping`);
-//     },
-// };
+module.exports = {
+    name: 'ping',
+    aliases: ['p'],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+        await reply(message, embedResponse({ color: '#59C2FF', msg: bold('Pong!') }));
+    },
+};
